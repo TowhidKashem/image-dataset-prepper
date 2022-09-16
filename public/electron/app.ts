c12d25dbe95c7db2d486c3882d8a201659c6d5af
TowhidKashem <tkashem@gmail.com>
2022-09-16 00:03:03 -0500

    work

--- public/electron/app.ts
+++ public/electron/app.ts
@@ -1,12 +1,15 @@
+const fs = require('fs');
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  contextBridge,
+  ipcRenderer
+} = require('electron');
 const isDev = require('electron-is-dev');
-const api = require('./api');
-
-api();
 
 function createWindow() {
-  // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -15,14 +18,12 @@ function createWindow() {
     }
   });
 
-  // and load the index.html of the app.
-  // win.loadFile("index.html");
   win.loadURL(
     isDev
       ? 'http://localhost:3000'
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
-  // Open the DevTools.
+
   if (isDev) {
     win.webContents.openDevTools({ mode: 'right' });
   }
@@ -30,12 +31,12 @@ function createWindow() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Some APIs can only be used after this event occurs
 app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// explicitly with Cmd + Q
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -45,5 +46,60 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
+  }
+});
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    sendMessage(channel, args) {
+      ipcRenderer.send(channel, args);
+    },
+    on(channel, func: (...args) => void) {
+      const subscription = (_event, ...args) => func(...args);
+      ipcRenderer.on(channel, subscription);
+      return () => ipcRenderer.removeListener(channel, subscription);
+    },
+    once(channel, func: (...args) => void) {
+      ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    }
+  }
+});
+
+//*--------------------------------------- endpoints ---------------------------------------*//
+
+ipcMain.on('get_all_images', async (event, args) => {
+  try {
+    const images = fs.readdirSync(args.directory);
+    event.reply('get_all_images', images);
+  } catch (error) {
+    event.reply('get_all_images', {
+      error,
+      directory: args.directory
+    });
+  }
+});
+
+ipcMain.on('get_image', async (event, args) => {
+  try {
+    const file = args.directory + '/' + args.filename;
+    const image = fs.readFileSync(file);
+
+    event.reply('get_image', image.toString('base64'));
+  } catch (error) {
+    event.reply('get_all_images', error);
+  }
+});
+
+ipcMain.on('delete_image', async (event, args) => {
+  try {
+    const file = args.directory + '/' + args.filename;
+    fs.unlinkSync(file);
+
+    event.reply('delete_image', { success: true });
+  } catch (error) {
+    event.reply('delete_image', {
+      success: false,
+      error
+    });
   }
 });
