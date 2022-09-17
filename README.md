d4c0e280734462d1efeb28bf10e71050ed2e867f
TowhidKashem <tkashem@gmail.com>
2022-09-17 15:28:30 -0500

    work

---
+++ README.md
@@ -0,0 +1,25 @@
+# Image Reviewer GUI
+
+Install and run the dev server:
+
+```
+> yarn install
+> yarn start
+```
+
+Build the app as a standalone portable on mac:
+
+```
+> yarn build
+```
+
+## Usage
+
+- <kbd>→</kbd> next image
+- <kbd>←</kbd> prev image
+- <kbd>Space</kbd> delete image
+
+## Files
+
+- Electron backend - `public/electron/*`
+- React frontend - `src/*`
--- public/electron/app.ts
+++ public/electron/app.ts
@@ -1,48 +1,54 @@
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 const { app, BrowserWindow, ipcMain } = require('electron');
-const isDev = require('electron-is-dev');
+
+const isDev = process.env.NODE_ENV === 'development';
 
 function createWindow() {
-  const win = new BrowserWindow({
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, '../logo192.png'),
+    icon: path.join(__dirname, 'icon.png'),
     webPreferences: {
       nodeIntegration: true,
       preload: path.join(__dirname, 'preload.ts')
     }
   });
 
-  win.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
+  //  `file://${__dirname}/../build/index.html`
 
   if (isDev) {
-    win.webContents.openDevTools({ mode: 'right' });
+    mainWindow.loadURL('http://localhost:3000');
+  } else {
+    mainWindow.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'dist', 'mac', 'index.html'),
+        protocol: 'file',
+        slashes: true
+      })
+    );
   }
+
+  // if (isDev) {
+  mainWindow.webContents.openDevTools({ mode: 'right' });
+  // }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 
-app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
 });
 
 //*--------------------------------------- endpoints ---------------------------------------*//
