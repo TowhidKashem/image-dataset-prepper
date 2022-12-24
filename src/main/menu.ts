a978a6de4535846f1d8f0b7dff8c881f9e8e955a
TowhidKashem <tkashem@gmail.com>
2022-12-24 14:52:32 -0600

    work

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About ImageReviewer',
           selector: 'orderFrontStandardAboutPanel:'
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide ImageReviewer',
           accelerator: 'Command+H',
           selector: 'hide:'
         },
